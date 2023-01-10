5e962345325657b95820f9a910392b9768365641
Sharath <sharathpandi.s@dreamtusk.com>
2023-01-10 10:12:36 +0530

    Code Corrections

--- problem_one/bin/problem_one.dart
+++ problem_one/bin/problem_one.dart
@@ -2,7 +2,7 @@ import 'package:problem_one/problem_one.dart' as problem_one;
 
 void main(List<String> arguments) {
   // print('Hello world: ${problem_one.calculate()}!');
-  print(_twoSum([6, 4, 6], 12));
+  print(_twoSum([3, 3], 6));
 }
 
 List<int> _twoSum(List<int> num, int target) {
@@ -13,16 +13,15 @@ List<int> _twoSum(List<int> num, int target) {
       int element2 = num[j];
       int sum = element1 + element2;
       bool _elementsPosition1 =
-          num.indexOf(element1) != num.indexOf(element2) ? true : false;
-
-      bool _elementsPosition2 =
-          num.indexOf(element1) != num.lastIndexOf(element2) ? true : false;
+          // num.indexOf(element1) != num.indexOf(element2) ? true : false;
+          i != j ? true : false;
+      bool _elementsPosition2 = i != num.lastIndexOf(element2) ? true : false;
       if (sum == target && _elementsPosition1) {
-        _result.add(num.indexOf(element1));
-        _result.add(num.indexOf(element2));
+        _result.add(i);
+        _result.add(j);
         return _result;
       } else if (sum == target && _elementsPosition2) {
-        _result.add(num.indexOf(element1));
+        _result.add(i);
         _result.add(num.lastIndexOf(element2));
         return _result;
       }
