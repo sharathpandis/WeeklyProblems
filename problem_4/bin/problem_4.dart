05167622350efc827c6c87fc358652718a6f8961
Sharath <sharathpandi.s@dreamtusk.com>
2023-01-20 14:14:55 +0530

    Bug fixed

--- problem_4/bin/problem_4.dart
+++ problem_4/bin/problem_4.dart
@@ -2,16 +2,20 @@ import 'package:problem_4/problem_4.dart' as problem_4;
 
 void main(List<String> arguments) {
   // print('Hello world: ${problem_4.calculate()}!');
-  print(_checkIp4Address("255.0.18.22"));
+  print(_checkIp4Address("255.0.1.22"));
 }
 
 String _checkIp4Address(String ip4Address) {
   List address = ip4Address.split(".");
   String result = "";
+  if (ip4Address.contains(RegExp(r'[A-Z]')) ||
+      ip4Address.contains(RegExp(r'[a-z]'))) {
+    return "Invalid";
+  }
   for (var i = 0; i < address.length; i++) {
     int element = int.parse(address[i]);
     if (element > 255) {
-      result = "not Valid";
+      result = "Invalid";
       break;
     } else {
       result = "Valid";
